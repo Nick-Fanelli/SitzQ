d82060938b3f74b4f84e3474e3652b5997a51e65
Nick Fanelli <nickfanelli5@gmail.com>
2023-11-22 18:45:13 -0500

    feat: add local audio playback feature

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,9 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-#[tauri::command]
-fn test_command() {
-    println!("Hello World");
-}
-
 fn main() {
-    tauri::Builder::default()
 
-        .invoke_handler(tauri::generate_handler![test_command])
+    tauri::Builder::default()
 
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,52 @@
+import { invoke } from "@tauri-apps/api";
+import { open } from "@tauri-apps/api/dialog"
+import { convertFileSrc } from '@tauri-apps/api/tauri';
+import { useCallback, useEffect, useState } from "react"
+
 const App = () => {
 
+    const [soundFile, setSoundFile] = useState<string | null>(null);
+
+    const openSoundFile = useCallback(() => {
+
+        open({
+            multiple: false,
+        }).then((res) => {
+            if(res !== null) {
+
+                setSoundFile(convertFileSrc(res.toString()));
+
+            }
+        });
+
+    }, [setSoundFile]);
+
+    const playSound = useCallback(() => {
+
+        if(soundFile === null)
+            return;
+
+        let audio = new Audio(soundFile);
+        audio.play();
+
+        console.log(soundFile);
+
+    }, [soundFile]);
+
+    useEffect(() => {
+
+
+        // let a = new Audio("file://Users/nickfanelli/Downloads/audio/A Really Short Slow Dance Song (revised 2).wav");
+        // a.play();
+
+    }, [soundFile]);
+
     return (
-        "Hello World"
+        <>
+            <h1>{soundFile}</h1>
+            <button onClick={openSoundFile}>Open File</button>
+            <button onClick={playSound}>Play Sound File</button>
+        </>
     )
 
 }
