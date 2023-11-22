c9f5a27b909f52a2796cc880ea28077a3356a674
Nick Fanelli <nickfanelli5@gmail.com>
2023-11-21 20:34:16 -0500

    feat: add invoke and tauri rust setup

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,16 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+#[tauri::command]
+fn test_command() {
+    println!("Hello World");
+}
+
 fn main() {
     tauri::Builder::default()
+
+        .invoke_handler(tauri::generate_handler![test_command])
+
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
