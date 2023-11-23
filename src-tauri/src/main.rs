81c5da83789e492720a2c6735519249fd6b5335f
Nick Fanelli <nickfanelli5@gmail.com>
2023-11-22 20:15:03 -0500

    fix: play audio from rust backend instead of frontend

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,9 +1,51 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::fs::File;
+use std::io::BufReader;
+
+use rodio::{Sink, OutputStream};
+
+use tauri::State;
+
+#[tauri::command]
+fn play_sound(audio_path: String, state: State<AppState>) {
+
+    println!("Playing Sound File: {}", audio_path);
+
+    let file = File::open(audio_path).expect("Error opening filepath");
+    let source = rodio::Decoder::new(BufReader::new(file)).expect("Error loading source");
+
+    state.sink.append(source);
+
+}
+
+struct AppState {
+    sink: Sink,
+}
+
+impl AppState {
+    fn new(sink: Sink) -> Self {
+        AppState { sink }
+    }
+}
+
 fn main() {
 
+    // let device = cpal::default_host().default_output_device().expect("Error getting default output device");
+
+    // Create an output stream
+    let (_stream, stream_handle) = OutputStream::try_default().expect("Error creating output stream");
+
+    // Play the sound on the default device
+    let sink = Sink::try_new(&stream_handle).expect("Error creating sink");
+
+    let app_state = AppState::new(sink);
+
     tauri::Builder::default()
+        .manage(app_state)
+
+        .invoke_handler(tauri::generate_handler![play_sound])
 
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { invoke } from "@tauri-apps/api";
 import { open } from "@tauri-apps/api/dialog"
-import { convertFileSrc } from '@tauri-apps/api/tauri';
-import { useCallback, useEffect, useState } from "react"
+import { useCallback, useState } from "react"
 
 const App = () => {
 
@@ -13,39 +12,17 @@ const App = () => {
             multiple: false,
         }).then((res) => {
             if(res !== null) {
-
-                setSoundFile(convertFileSrc(res.toString()));
-
+                setSoundFile(res.toString());
             }
         });
 
     }, [setSoundFile]);
 
-    const playSound = useCallback(() => {
-
-        if(soundFile === null)
-            return;
-
-        let audio = new Audio(soundFile);
-        audio.play();
-
-        console.log(soundFile);
-
-    }, [soundFile]);
-
-    useEffect(() => {
-
-
-        // let a = new Audio("file://Users/nickfanelli/Downloads/audio/A Really Short Slow Dance Song (revised 2).wav");
-        // a.play();
-
-    }, [soundFile]);
-
     return (
         <>
             <h1>{soundFile}</h1>
             <button onClick={openSoundFile}>Open File</button>
-            <button onClick={playSound}>Play Sound File</button>
+            <button onClick={() => invoke('play_sound', { audioPath: soundFile })}>Play Sound File</button>
         </>
     )
 
